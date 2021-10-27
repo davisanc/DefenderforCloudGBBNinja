4bb397584d874167a6f623dbf12666cc4f4e9adb
carlosfar <63407805+carlosfar@users.noreply.github.com>
2021-10-27 14:55:34 +0100

    Add files via upload

---
+++ 07-KeyVault/KayVault_TorAccess.ps1
@@ -0,0 +1,6 @@
+### ---------------------------------------------------------------------------------
+### ASC Alert simulation for KeyVault
+### ---------------------------------------------------------------------------------
+
+# Simple test, to access KeyVault from Tor and generate alert
+https://techcommunity.microsoft.com/t5/azure-security-center/validating-azure-key-vault-threat-detection-in-azure-security/ba-p/1220336
---
+++ 09-AppService/AppService_SampleAlert.ps1
@@ -0,0 +1,6 @@
+### ---------------------------------------------------------------------------------
+### ASC Alert simulation for App Service to Generate Sample alert
+### ---------------------------------------------------------------------------------
+
+# Simple test, to access URL that will generate alert for ASC. More info:
+https://techcommunity.microsoft.com/t5/azure-security-center/validating-azure-defender-for-app-service-alerts/ba-p/2656230
